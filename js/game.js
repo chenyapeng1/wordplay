bada424bfdf8d643c704808ba9c4df3ce554d5b7
chenyapeng1 <18335442530@163.com>
2018-08-06 16:37:29 +0800

    cyp

--- js/game.js
+++ js/game.js
@@ -5,42 +5,48 @@ class Game{
         this.jf="";
         this.lift="";
         this.flag="";
-        this.letterBox=[];//[{"name":"A",left:1.8,top:2,"node":node},{},{},{},{}]          数据类型
+        this.letterBox=[];//[{"name":"A",left:1.8,top:2,"node":node},{},{},{},{}]          生成的字母的名字,距离左边的距离,距离顶部的高度,节点
         this.letterWidth=0.53;
+        this.fractionnum=0;
+        this.lifenum=10;
+        this.death="";
+        this.textnum=0;
+        this.replay="";
+        this.sudu=0.01;
     }
-    createLetter(num=1){
+    createLetter(num=1){//括号中是默认值
         for(let i=0;i<num;i++){
             //创建字母,1,保存到数据中2,插入到页面中
             //注意,字母重复和重叠;,重叠看left
-            let obj={};
-            let letter="";
+            let obj={};//letterBox中的每一个数,数据类型是字符串
+            let letter="";//letter是一个随机生成的字母
             do{
                 //创建字母name
                 let ascii = Math.floor(Math.random()*26+65);//a-z的ascii码值
-                letter=String.fromCharCode(ascii);
-            }while(this.isHas(letter));//do while,至少循环一次
+                letter=String.fromCharCode(ascii);//fromCharCode是将ascii码值转换为对应的字符
+            }while(this.isHas(letter));//do while,至少循环一次,
             //this.isHas    作用,判断是否重复,是,返回true,
 
 
+            obj.name=letter;//将随机生成的字母存放到obj中的name属性中
+           
 
-            obj.name=letter;
-
-            let div=document.createElement("div");
+            let div=document.createElement("div");//创立一个div的节点
             //添加字母样式
-            div.className="letter";
-            div.style.backgroundImage=`url(img/A_Z/${letter}.png)`;
+            div.className="letter";//将创立的节点的类名改变为字母的名字,注意:类名是一个字符串
+            div.style.backgroundImage=`url(img/A_Z/${letter}.png)`;//改变背景图,让每一个生成的字母的背景图都是对应的那个字母
 
             let left="";
             do{
                 left=Math.random()*4+1;
-            }while(this.isRepeat(left));
+            }while(this.isRepeat(left));//判断字母是否重叠,重叠的话再生成.
 
 
 
             div.style.left=left+"rem";
             this.screen.appendChild(div);
             //保存数据
-            obj.name=name;
+            name=obj.name;
             obj.left=left;
             obj.top=1;
             obj.node=div;
@@ -70,4 +76,68 @@ class Game{
     //消失
     //游戏开始
     //重新开始
+
+    init(){
+        // this.screen="";//构造函数,自动运行
+        // this.music="";
+        this.jf="";
+        this.lift="";
+        this.flag="";
+        this.letterBox=[];//[{"name":"A",left:1.8,top:2,"node":node},{},{},{},{}]          生成的字母的名字,距离左边的距离,距离顶部的高度,节点
+        this.letterWidth=0.53;
+        // this.fractionnum=0;
+        this.lifenum=10;
+        // this.death="";
+        this.textnum=0;
+        this.replay="";
+        this.life.innerText=10;
+        this.sudu=0.01;
+    }
+    run(){
+        let that=this;
+        this.t=setInterval(function(){
+            that.letterBox.forEach(function(item,index){
+                item.top+=that.sudu;
+                if(item.top>8){
+                    that.screen.removeChild(item.node);
+                    that.letterBox.splice(index,1);
+                    that.createLetter(1);
+                    that.lifenum--;
+                    that.sublife();
+                    if(that.lifenum<=0){
+                        that.death.style.display="block";
+                        clearInterval(that.t)
+                        that.letterBox.forEach((item,index)=>{
+                            that.screen.removeChild(item.node);
+                        })
+                        that.init();
+                    }
+                }
+                item.node.style.top=item.top+"rem";
+            })
+        },10)
+    }
+    pause(){
+        clearInterval(this.t);
+    }
+
+    ontouch(name){
+        this.letterBox.forEach((item,index)=>{
+            if (item.name==name){
+                this.screen.removeChild(item.node);
+                this.letterBox.splice(index,1);
+                this.createLetter();
+                this.fractionnum++;
+                this.sudu=this.fractionnum/1000+0.01;
+                this.addfraction();
+            }
+        })
+    }
+    addfraction(){
+        this.fraction.innerText=this.fractionnum;//积分值与最终得分
+        this.text.innerText=this.fractionnum;
+    }
+    sublife(){
+        this.life.innerText=this.lifenum;//生命值
+    }
 }
--- js/index.js
+++ js/index.js
@@ -1,49 +1,72 @@
 window.onload=function(){
-    //声音的样式的变化
     let music=document.querySelector("#music");//声音
     let nei=document.querySelector(".nei");//按键
     let goon=document.querySelector("#goon");//暂停
-    let screen=document.querySelector(".screen");
-    let left=document.querySelector(".left");
+    let screen=document.querySelector(".screen");//屏幕
+    let life=document.querySelector(".life");
+    let fraction=document.querySelector(".fraction");
+    let death=document.querySelector(".death");
+    let text=document.querySelector(".death .text");
+    let replay=document.querySelector(".replay");
+    let audio=document.querySelector("#audio");
+    //声音的样式的变化
     music.ontouchend=function(){
         if (music.className=="musicss"){
             music.className="musics";
+            audio.pause();
         }
         else{
             music.className="musicss";
-        }
+            audio.play();
+        }//创立两个class,如果点击的是第一个class就让class变为第二个.而找这个div是用id来找的,因为id是唯一的
     }
     //键盘每个键的大小的变化
     
     nei.ontouchstart=function(e){
         if(e.target.className=="son"){
             e.target.style.transform="scale(0.8)";
+            gameobj.ontouch(e.target.innerText);
         }
-    }
+    }//鼠标按下
     nei.ontouchend=function(e){
         if(e.target.className=="son"){
             e.target.style.transform="scale(1)";
         }
-    }
+    }//鼠标抬起
     //暂停和继续
     
     goon.ontouchend=function(){
         if (goon.className=="goon1"){
             goon.className="goon2";
+            gameobj.run();
         }
         else{
             goon.className="goon1";
+            gameobj.pause();
         }
     }
 
+    replay.ontouchstart=function(){
+        death.style.display="none";
+        gameobj.screen=screen;
+        gameobj.createLetter(5);
+        gameobj.run();
+        // game.init();
+    }
 
 
-
-    let gameobj=new Game;
+    let gameobj=new Game;//将game.js引入到此文档中,调用       创建对象
     gameobj.screen=document.querySelector(".screen");
     // gameobj.flag=flag;
     gameobj.music=music;
     gameobj.lift=length;
-    // gameobj.fraction=fraction;
+    gameobj.life=life;
+    gameobj.fraction=fraction;
+    gameobj.death=death;
+    gameobj.text=text;
+    gameobj.replay=replay;
+    gameobj.init();
     gameobj.createLetter(5);
+    gameobj.run();
+    gameobj.addfraction();
 }
